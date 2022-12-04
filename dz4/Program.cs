6a8a6d6f7aa5392d3d35775de5bb35d9bcb0e6bc
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2022-12-05 01:26:03 +0600

    задача вывод четных чисел от 1 до N

---
+++ dz4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число = ");
+string enterNum = Console.ReadLine();
+int number = Convert.ToInt32(enterNum);
+int i = 1;
+int count = 0;
+int result2 = 0;
+    while (count < number)
+{
+    int result =  i % 2;
+    if (result == 0)
+    {
+     Console.Write($"{i}, ");
+    }   
+    i++; 
+    count++;
+};
