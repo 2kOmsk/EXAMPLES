0ed495351caced46ffe0c53b74daea680716d0b3
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2022-12-04 23:49:52 +0600

    зачада на  четное  не  четно число размер int32

---
+++ dz3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число отличное от 0 = ");
+string enterNum = Console.ReadLine();
+int number = Convert.ToInt32(enterNum);
+int i = 2;
+int j = number;
+int result =  j % i;
+if (result == 0)
+{
+Console.WriteLine($"число {number} четное");
+}
+else 
+{
+Console.WriteLine($"число {number} не четное");
+};
+
