63c5835254c111b35054ff94f87f0b15b40df52f
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2022-12-04 19:30:16 +0600

    dz min/max

---
+++ dz1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите a = ");
+string enterNumA = Console.ReadLine();
+int numA = Convert.ToInt32(enterNumA);
+Console.Write("Введите b = ");
+string enterNumB = Console.ReadLine();
+int numB = Convert.ToInt32(enterNumB);
+int maxNum = 0;
+if (numA < numB)
+{
+    Console.WriteLine($"maxNum = {numB} ");
+}
+else 
+{
+    Console.WriteLine($"maxNum = {numA} ");
+};
