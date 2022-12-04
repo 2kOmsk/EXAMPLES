aacb4dca0960ba6f9c6f73b2a01e6f725f398c07
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2022-12-04 22:09:14 +0600

    максимальное  из 3 чисел

---
+++ dz2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите a = ");
+string enterNumA = Console.ReadLine();
+int numA = Convert.ToInt32(enterNumA);
+Console.Write("Введите b = ");
+string enterNumB = Console.ReadLine();
+int numB = Convert.ToInt32(enterNumB);
+Console.Write("Введите c = ");
+string enterNumC = Console.ReadLine();
+int numC = Convert.ToInt32(enterNumC);
+int maxNum = 0;
+if (numA < numB)
+{
+  maxNum = numB;
+}
+else if (numA > numB)
+{
+  maxNum = numA;
+}
+if (maxNum < numC)
+{
+    Console.WriteLine($"maxNum = {numC} ");
+}
+else 
+{
+    Console.WriteLine($"maxNum = {maxNum} ");
+};
