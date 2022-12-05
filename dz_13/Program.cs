ce2e6bb7c410beb5ac6716b146c3fe27bb2d16f4
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2022-12-06 02:17:34 +0600

    3-я цифра числа или ее отсутствие

---
+++ dz_13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число от 3-х символов: ");
+string nuM = Console.ReadLine();
+int lenght = Convert.ToInt32(nuM.Length);
+int index = 3;
+if (lenght < index )
+{
+    Console.WriteLine("вы ввели число менее 3-х символов");
+}
+else
+{
+    Console.WriteLine(nuM[2]);
+};
