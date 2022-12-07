65d04532ccdd98c76fe924a2bb8f812eeac1bc6f
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2022-12-07 20:56:13 +0600

    Дни недели

---
+++ dz_15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число обозначающее день недели: ");
+string userday = Console.ReadLine();
+int findday = Convert.ToInt32(userday);
+int [] week = {1,2,3,4,5};
+int days = week.Length;
+int index = 4;
+{
+    if (findday <= days)
+    {
+        Console.WriteLine("Рабочийдень");
+    }
+    else
+    {
+        Console.WriteLine("Выходной день");
+    }
+};
+
+
+
+//int [] rab = new int {1,2,3,4,5};
+//int [] vix = new int {6,7};
