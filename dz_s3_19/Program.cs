fec0f8d926c1ebdfbc1369a1b8332f132c1b07c3
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2022-12-10 23:30:26 +0600

    new

--- dz_s3_19/Program.cs
+++ dz_s3_19/Program.cs
@@ -1,27 +1,21 @@
-﻿// Задача 19
-
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
-// 14212 -> нет
-
-// 12821 -> да
-
-// 23432 -> да
-
-
-
+﻿
 Console.WriteLine("Введите 5 - значное число : ");
 int usernamber = Convert.ToInt32(Console.ReadLine());
 int [] arrayuser = new int [usernamber];
-int length = arrayuser.Length;
+
 
 int [] polyarray = new int[5];
 
-for (int i = 0; i < length; i++)
+for (int i = 0; i < arrayuser.Length; i++)
 {
-    int j = length - i;
-    polyarray[j] = arrayuser[i];
-    i++;
+    for (int j = 0; j < polyarray.Length  ; j++)
+    {
+        int temp = arrayuser.Length - j;
+        polyarray[temp] = arrayuser[i];
+        Console.WriteLine(polyarray[temp]);
+    }
+    
+
 }
 Console.WriteLine(polyarray);
 int age = Convert.ToInt32(Console.ReadLine());
