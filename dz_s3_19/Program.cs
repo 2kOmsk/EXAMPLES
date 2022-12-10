766a12ccd8ff81c664ce17584624a5d3efcb0832
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2022-12-10 22:21:58 +0600

    задача 19 перевернуть полиндром

---
+++ dz_s3_19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+
+Console.WriteLine("Введите 5 - значное число : ");
+string usernamber = Console.ReadLine();
+int zzz = Convert.ToInt32(usernamber);
+int [] arrayuser = new int [zzz];
+int length = arrayuser.Length;
+
+int [] polyarray = new int[5];
+
+for (int i = 0; i < length; i++)
+{
+    int j = length - i;
+    polyarray[j] = arrayuser[i];
+    i++;
+}
+Console.WriteLine(polyarray);
